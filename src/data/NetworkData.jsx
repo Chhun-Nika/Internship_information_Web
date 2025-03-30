65e99b848e3ab9dad8f094e1abbb69527bc32d49
Sotheara Kon <sothearakon73@gmail.com>
2025-03-30 14:49:28 +0700

    all department page

---
+++ src/data/NetworkData.jsx
@@ -0,0 +1,91 @@
+import smartImage from '../assets/smart.png';
+import ABAImage from '../assets/aba.png';
+import chipmongImage from '../assets/Chipmong.png';
+import networkPic from '../assets/networkPic.png';
+
+
+const NetworkData = [
+    {
+        logo : smartImage,
+        company : "Smart Anxiata",
+        picture : networkPic,
+        tittle : "Network Engineer",
+        desciption : "Assist in configuring and maintaining 4G/5G networks, troubleshoot connectivity issues, and optimize telecom performance.",
+        category : "Telecom & NetWorking"
+    },
+
+    {
+        logo : smartImage,
+        company : "Smart Anxiata",
+        picture : networkPic,
+        tittle : "Telecom Engineer",
+        desciption : "Work on radio frequency (RF) planning, fiber-optic networks, and telecom infrastructure expansion.",
+        category : "Telecom & NetWorking"
+    },
+
+    {
+        logo : smartImage,
+        company : "Smart Anxiata",
+        picture : networkPic,
+        tittle : "IoT & Smart Systems",
+        desciption : "Develop IoT-based telecom solutions for smart cities, industrial automation, and connected devices.",
+        category : "Cyber Security"
+    },
+
+    {
+        logo : smartImage,
+        company : "Smart Anxiata",
+        picture : networkPic,
+        tittle : "Cloud Network",
+        desciption : "Work on cloud-based financial systems, ensuring smooth and secure digital banking services.",
+        category : "Telecom & NetWorking"
+    },
+
+    {
+        logo : ABAImage,
+        company : "ABA Bank",
+        picture : networkPic,
+        tittle : "Cybersecurity",
+        desciption : "Protect banking infrastructure, configure firewalls, and prevent cyber threats.",
+        category : "Cyber Security"
+    },
+
+    {
+        logo : ABAImage,
+        company : "ABA Bank",
+        picture : networkPic,
+        tittle : "AI & Machine",
+        desciption : "Build AI models for financial forecasting, fraud detection, and risk assessment in banking.",
+        category : "Cyber Security"
+    },
+
+    {
+        logo : chipmongImage,
+        company : "Chip Mong group",
+        picture : networkPic,
+        tittle : "IT Network Support",
+        desciption : "Maintain network connectivity across Chip Mong’s offices, retail branches, and warehouses.",
+        category : "Telecom & NetWorking"
+    },
+
+    {
+        logo : chipmongImage,
+        company : "Chip Mong group",
+        picture : networkPic,
+        tittle : "AI & Machine",
+        desciption : "Build AI models for financial forecasting, fraud detection, and risk assessment in banking.",
+        category : "Cyber Security"
+    },
+
+    {
+        logo : chipmongImage,
+        company : "Chip Mong group",
+        picture : networkPic,
+        tittle : "Security & Compliance",
+        desciption : "Monitor network security policies, ensuring compliance with IT regulations.",
+        category : "Cyber Security"
+    },
+
+]
+
+export default NetworkData;
--- src/pages/Business.jsx
+++ src/pages/Business.jsx
@@ -1,12 +1,67 @@
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import React, { useState } from "react";
+import SearchBar from "../components/Search";
+import ItemBox from "../components/ItemBox"; 
+import BusinessData from "../data/BusinessData";
 
 const Business = () => {
+  const [category, setCategory] = useState("All");
+  const [searchTerm, setSearchTerm] = useState("");
+
+ 
+  const filteredData = (BusinessData || []).filter((job) => {
+    const matchesCategory = category === "All" || job.category === category;
+    const matchesSearch =
+      searchTerm === "" ||
+      job.tittle.toLowerCase().includes(searchTerm.toLowerCase()) ||
+      job.company.toLowerCase().includes(searchTerm.toLowerCase());
+
+    return matchesCategory && matchesSearch;
+  });
+
   return (
     <div className="min-h-screen flex flex-col">
       <Header />
       <main className="p-8">
-        
+        {/* Search Bar */}
+        <div>
+          <SearchBar 
+            title="Digital Business" 
+            description="Unlock diverse internship opportunities in Digital Business, developing expertise in e-commerce, digital marketing, fintech, and data-driven business strategies."
+            placement="items-center text-center"
+            placeholder="Search company name or job title"
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+        </div>
+
+        <div className="main-color text-2xl font-semibold font-quicksand pt-15 pb-5">
+          <h1>Based on your Specialization</h1>
+        </div>
+
+        <div className="flex space-x-4 justify-start my-4 font-quicksand pb-10">
+            <button className="px-4 py-2 rounded bg-[#628ace] text-white font-medium shadow-[0px_2px_6px_rgba(0,0,0,0.3)]" > 
+              e-commerce
+            </button>
+        </div>
+
+        {/* Internship Cards Grid */}
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+          {filteredData.length > 0 ? (
+            filteredData.map((job, index) => (
+              <ItemBox
+                key={index}
+                logo={job.logo}
+                company={job.company}
+                picture={job.picture}
+                title={job.tittle}
+                description={job.desciption}
+              />
+            ))
+          ) : (
+            <p className="text-center text-gray-500 col-span-3">No internships found.</p>
+          )}
+        </div>
       </main>
       <Footer />
     </div>
